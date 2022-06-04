1cc8c717fdf77282c6098f57d38091935ea8e631
HakuSystems <66133638+HakuSystems@users.noreply.github.com>
2022-06-04 21:15:03 +0200

    Add project files.

---
+++ ServerFileManager/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Shapes;
+
+namespace ServerFileManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            statusTxt.Text = "Please Wait";
+            //get current application path
+            var currentDir = Directory.GetCurrentDirectory();
+        }
+    }
+}
